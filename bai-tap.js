a493050e397a30cd4c98e5882e54503fc32d545b
Lee Yan Ji <pnk.leeyanji@gmail.com>
2019-03-01 08:27:12 +0700

    push bai tap

---
+++ bai-tap.js
@@ -0,0 +1,21 @@
+//tinh trung binh
+let vatly = prompt("Nhap diem Vat Ly");
+let hoahoc = prompt("Nhap diem Hoa Hoc");
+let sinhhoc = prompt("Nhap diem Sinh Hoc");
+let trungbinh = (vatly + hoahoc + sinhhoc)/3
+Document.write("Diem trung binh la:"+ trungbinh);
+//Chuyen C=>F
+let C = prompt("Nhap do C");
+let F = (C*0.8)+32;
+Document.write(C + "C bang" + F + "F");
+//tinh dien tich hinh tron
+let r = prompt("Nhap ban kinh");
+let pi = 3.14;
+let S = pi*r*r;
+Document.write("S="_+S);
+//tinh chu vi
+let r = prompt("Nhap ban kinh");
+let pi = 3.14;
+let C = 2*r*pi;
+Document.write("C="_+C);
+
